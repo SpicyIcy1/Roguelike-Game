b4657c83580cd3a0da22a0b3571a0ca154da1dce
Loewe121919 <itch.gruffly006@passinbox.com>
2026-07-03 09:33:35 +0200

    Truhe öffnen

--- info/scripts/truhe.gd
+++ info/scripts/truhe.gd
@@ -1,9 +1,16 @@
-extends Node2D
+extends Entity
 
 var opened = false
 
+func take_damage(amount: float) -> void:
+	if not opened:
+		open()
+		opened = true
+
+
 func open():
 	$Sprite2D.region_rect = Rect2(Vector2(20,0), Vector2(20,27))
+	PlayerData.add_morality(5)
 
 func close():
 	$Sprite2D.region_rect = Rect2(Vector2(0,0), Vector2(20,27))
