bcdba6eb8eed9e420d0829cff66a713a2989f237
Loewe121919 <halbhleo@gym-hksb.de>
2026-07-05 22:13:49 +0200

    Explosion upon boss defeat

--- info/scripts/DragonWorm/room_boss.gd
+++ info/scripts/DragonWorm/room_boss.gd
@@ -4,10 +4,12 @@ extends Node2D
 
 @onready var boss : = $DragonWorm
 
+
 func _ready():
 	print("FIGHT!")
 	boss.tree_exited.connect(_on_boss_disappeared)
 
 func _on_boss_disappeared():
 	$Wall.queue_free()
+	$AnimationPlayer.play("explosion")
 	print("Killed")
